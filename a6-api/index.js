febe3c54ab5dfe60ddeb610dd64d407e3a8936a6
Jesse Hernandez <37102346+jesse-hdz@users.noreply.github.com>
2018-06-12 11:59:47 -0700

    a6-api upload

---
+++ a6-api/index.js
@@ -0,0 +1,184 @@
+/**
+ * index.js -- a6-api
+ * Each route invokes the corresponding method in Team.js module 
+ * and sends the returned value on the server response.
+ *
+ * @author Jesse Hernandez
+ * @see Team.js
+ * @see api.handlebars
+ * @see add.handlebars
+ * @see 404.handlebars
+ * @see 500.handlebars
+ */
+
+const express = require('express'),
+    handlebars = require('express-handlebars').create({ defaultLayout: 'main' }),
+    // parser = require('body-parser'),
+    // team = require('./lib/teams.js'),
+    Team = require("./models/Team.js"),
+    teamMethods = require("./teamMethods");
+
+const app = express();
+
+// set up handlebars view engine
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/views/public')); // set location for static files
+
+app.use(require("body-parser").urlencoded({ extended: true })); // parse form submissions
+
+
+// Routes
+// home / search bar page
+// app.get('/', (req, res) => {
+//     res.render('home');
+// });
+
+// home / search bar page (to use Teams db (mongoDb))
+app.get('/', (req, res, next) => {
+    teamMethods.getAll().then((teams) => {
+        res.render('home', { teams: teams });
+    }).catch((err) => {
+        return next(err);
+    });
+});
+
+// about page
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// Search for teams displayed on home page
+app.post('/search', (req, res, next) => {
+    let result = req.body.club;
+
+    Team.findOne({ title: result }, (err, club) => {
+        if (err) return next(err);
+        res.render('details', { result: club });
+
+    });
+});
+
+// query get page to retrieve specific team (when input in query string in url)
+app.get('/get', (req, res) => {
+    let result = Team.get(req.query.club);
+    res.render('details', { title: req.query.club, result: result });
+});
+
+// displays team information entered in search bar
+app.post('/details', (req, res, next) => {
+    Team.findOne({ club: (String(req.body.club)) }, (err, team) => {
+        if (err) return next(err);
+        res.render('details', { result: team });
+    });
+});
+
+// deletes team if button is clicked on 'details' page
+app.post('/delete', (req, res, next) => {
+    Team.delete({ club: req.body.club }, (err, result) => {
+        if (err) return next(err);
+        let deleted = result;
+
+        if (deleted) {
+            res.send('Team has been deleted: ', { club: req.body.club });
+        } else {
+            res.send('Team has not been deleted: ', { club: req.body.club });
+        }
+
+    });
+});
+
+// adds a team
+app.post('/add', (req, res) => {
+    let club = req.body.club,
+        sport = req.body.sport,
+        founded = req.body.founded,
+        image = "/img/home.jpg";
+
+    let add = true;
+    // add 'club', 'sport', 'founded', 'image' to array of teams    
+    let result = Team.add({ club, sport, founded, image });
+    res.render('details', {
+        title: result,
+        club: club,
+        sport: sport,
+        founded: founded,
+        image: image,
+        add: add
+    });
+});
+
+// API Routes
+// API - retrieves a single team
+app.get('/api/team/:club', (req, res) => {
+    let club = req.params.club;
+    Team.find({ 'club': club }, (err, result) => {
+        if (err) {
+            return res.status(500).send('Error occurred: database error.');
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+// API - retrieves all teams
+app.get('/api/teams', (req, res, next) => {
+    let t = Team.find((team) => {
+        team.club = req.params.club;
+        return {
+            club: team.club,
+            sport: t.sport,
+            founded: t.founded,
+            image: "/img/home.jpg"
+        };
+    });
+
+    if (!t) return next(); // will eventually fall through to 404
+    res.send(t);
+});
+
+// API - deletes a team
+app.get('/api/delete/:club', (req, res) => {
+    let club = req.params.club;
+    Team.remove({ 'club': club }, (err, result) => {
+        if (err) {
+            return res.status(500).send('Error occurred: database error.');
+        } else {
+            res.json({ 'deleted': result });
+        }
+    });
+});
+
+// API - adds a team
+app.post('/api/add', (req, res) => {
+    let t = new Team({
+        club: req.body.club,
+        sport: req.body.sport,
+        founded: req.body.founded,
+        image: "/img/home.jpg",
+        approved: false
+    });
+    t.save((err, t) => {
+        if (err) return res.status(500).send('Error occurred: database error.');
+        res.json({ id: t._id, });
+    });
+});
+
+// define 404 handler
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+// 500 error handler (middleware) 
+app.use((err, req, res) => {
+    Error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started on http://localhost: ' + app.get('port') + '; press Ctrl-C to terminate');
+});
---
+++ a6-api/models/Team.js
@@ -0,0 +1,28 @@
+/**
+ * Team.js -- a6-api
+ * Connection to mongoose and mongoDb database remotely
+ *
+ * @author Jesse Hernandez
+ * @see index.js
+ * @see credentials.js
+ * 
+ */
+
+const mongoose = require('mongoose');
+
+
+// connection to remote database on mLab
+let conn = mongoose.connection;
+conn.on('error', Error.bind(console, 'connection error:'));
+
+// define Team model in JSON key/value pairs
+// values indicate the data type of each key
+let mySchema = mongoose.Schema({
+    club: { type: String, required: true },
+    sport: String,
+    pubdate: Date,
+    image: String,
+    approved: Boolean
+});
+
+module.exports = mongoose.model('Team', mySchema);
